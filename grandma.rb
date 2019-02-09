6326dffb55edf0005a48b8a8eb79e0beecfd6ce4
Ali Madni <ali555m@hotmail.com>
2019-02-09 11:40:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,11 +3,9 @@ def speak_to_grandma(words)
   if
     words == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  end
   elsif
   words == words.upcase
   "NO, NOT SINCE 1938!"
-  end
   else
     "HUH?! SPEAK UP, SONNY!"
   end
